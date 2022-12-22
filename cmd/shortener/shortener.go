f1f14b965644410797916fed61e4a722fba25363
Kirill.Znamenskiy.pw <Kirill@Znamenskiy.pw>
2022-12-22 17:53:06 +0700

    Updated flag processing

--- cmd/shortener/shortener.go
+++ cmd/shortener/shortener.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"flag"
 	"github.com/Kirill-Znamenskiy/Shortener/internal/config"
 	"github.com/Kirill-Znamenskiy/Shortener/internal/handlers"
 	"github.com/Kirill-Znamenskiy/Shortener/internal/storage"
+	"github.com/spf13/pflag"
 	"log"
 	"net/http"
 )
@@ -12,15 +12,8 @@ import (
 func main() {
 
 	cfg := config.LoadFromEnv()
-
-	flag.StringVar(&cfg.ServerAddress, "a", cfg.ServerAddress, "server address")
-	flag.StringVar(&cfg.ServerAddress, "server-address", cfg.ServerAddress, "server address")
-	flag.StringVar(&cfg.BaseURL, "b", cfg.BaseURL, "base url")
-	flag.StringVar(&cfg.BaseURL, "base-url", cfg.BaseURL, "base url")
-	flag.StringVar(&cfg.StorageFilePath, "f", cfg.StorageFilePath, "storage file path")
-	flag.StringVar(&cfg.StorageFilePath, "storage-file-path", cfg.StorageFilePath, "storage file path")
-
-	flag.Parse()
+	config.DefineFlags(cfg)
+	pflag.Parse()
 
 	var stg storage.Storage
 	if cfg.StorageFilePath == "" {
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,14 +1,20 @@
 package config
 
 import (
+	"errors"
 	"github.com/caarlos0/env/v6"
+	"github.com/iancoleman/strcase"
+	"github.com/spf13/pflag"
 	"log"
+	"reflect"
+	"sort"
+	"strings"
 )
 
 type Config struct {
-	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
-	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	StorageFilePath string `env:"FILE_STORAGE_PATH"`
+	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"localhost:8080" flagName:"server-address" flagShortName:"a" flagUsage:"server address"`
+	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080" flagName:"base-url" flagShortName:"b" flagUsage:"base url"`
+	StorageFilePath string `env:"FILE_STORAGE_PATH" flagName:"storage-file-path" flagShortName:"f" flagUsage:"storage file path"`
 }
 
 func LoadFromEnv() *Config {
@@ -18,4 +24,92 @@ func LoadFromEnv() *Config {
 		log.Fatal(err)
 	}
 	return cfg
+}
+
+func DefineFlags(cfg *Config) *pflag.FlagSet {
+	return DefineFlagSet(pflag.CommandLine, cfg)
+}
+func DefineFlagSet(fs *pflag.FlagSet, cfg *Config) *pflag.FlagSet {
+	if fs == nil {
+		panic(errors.New("non-nil pflag.FlagSet expected"))
+	}
+
+	// because I'll take care of the sorting myself
+	fs.SortFlags = false
+
+	// RV = ReflectValue
+	cfgRV := reflect.Indirect(reflect.ValueOf(cfg))
+	cfgRType := cfgRV.Type()
+
+	flagNames := make([]string, 0, cfgRType.NumField())
+	flagShortNames := make([]string, 0, cfgRType.NumField())
+	type AnyFlagKit struct {
+		fieldInd       int
+		flagName       string
+		flagShortName  string
+		flagUsageValue string
+	}
+	anyFlagName2Kit := make(map[string]AnyFlagKit, cfgRType.NumField())
+	for fieldInd := 0; fieldInd < cfgRType.NumField(); fieldInd++ {
+		cfgStructField := cfgRType.Field(fieldInd)
+		cfgStructFieldTag := cfgStructField.Tag
+
+		flagName := cfgStructFieldTag.Get("flagName")
+		flagName = strings.TrimSpace(flagName)
+		if flagName == "-" {
+			continue
+		}
+		if flagName == "" {
+			flagName = strcase.ToDelimited(cfgStructField.Name, '-')
+		}
+
+		flagShortName := cfgStructFieldTag.Get("flagShortName")
+		flagShortName = strings.TrimSpace(flagShortName)
+
+		flagUsageValue := cfgStructFieldTag.Get("flagUsage")
+		flagUsageValue = strings.TrimSpace(flagUsageValue)
+
+		afk := AnyFlagKit{
+			fieldInd:       fieldInd,
+			flagName:       flagName,
+			flagShortName:  flagShortName,
+			flagUsageValue: flagUsageValue,
+		}
+
+		if flagShortName != "" {
+			flagShortNames = append(flagShortNames, flagShortName)
+			anyFlagName2Kit[flagShortName] = afk
+		} else {
+			flagNames = append(flagNames, flagName)
+			anyFlagName2Kit[flagName] = afk
+		}
+	}
+
+	sort.Strings(flagShortNames)
+	sort.Strings(flagNames)
+	anyFlagNames := append(flagShortNames, flagNames...)
+
+	for _, anyFlagName := range anyFlagNames {
+		afk := anyFlagName2Kit[anyFlagName]
+
+		cfgFieldRV := cfgRV.Field(afk.fieldInd)
+		cfgFieldValueInterface := cfgFieldRV.Interface()
+
+		cfgFieldValueAddrRV := cfgFieldRV.Addr()
+		cfgFieldValueAddrInterface := cfgFieldValueAddrRV.Interface()
+
+		switch cfgFieldValue := cfgFieldValueInterface.(type) {
+		case bool:
+			fs.BoolVarP(cfgFieldValueAddrInterface.(*bool), afk.flagName, afk.flagShortName, cfgFieldValue, afk.flagUsageValue)
+		case int:
+			fs.IntVarP(cfgFieldValueAddrInterface.(*int), afk.flagName, afk.flagShortName, cfgFieldValue, afk.flagUsageValue)
+		case string:
+			fs.StringVarP(cfgFieldValueAddrInterface.(*string), afk.flagName, afk.flagShortName, cfgFieldValue, afk.flagUsageValue)
+		default:
+			panic(errors.New("unexpected field type"))
+		}
+
+	}
+
+	return fs
 }
